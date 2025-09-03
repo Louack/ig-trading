d7ba5030e389e7be09ae9ba5086213b9f56ccf82
Louack <loic.briset@hotmail.fr>
2025-09-03 21:29:59 +0200

    testing IG API endpoints

--- settings.py
+++ settings.py
@@ -6,5 +6,35 @@ from pathlib import Path
 env_path = Path(os.getenv("ENV_PATH", "/etc/dev/ig_trading/.env"))
 load_dotenv(dotenv_path=env_path)
 
-IG_API_NAME = os.getenv("IG_API_NAME")
+IG_IDENTIFIER = os.getenv("IG_IDENTIFIER")
+IG_PASSWORD = os.getenv("IG_PASSWORD")
 IG_API_KEY = os.getenv("IG_API_KEY")
+
+IG_DEMO_API_KEY = os.getenv("IG_DEMO_API_KEY")
+IG_DEMO_IDENTIFIER = os.getenv("IG_DEMO_IDENTIFIER")
+IG_DEMO_PASSWORD = os.getenv("IG_DEMO_PASSWORD")
+
+IG_PROD_API_KEY = os.getenv("IG_PROD_API_KEY")
+IG_PROD_IDENTIFIER = os.getenv("IG_PROD_IDENTIFIER")
+IG_PROD_PASSWORD = os.getenv("IG_PROD_PASSWORD")
+
+
+BASE_URLS = {
+    "demo": "https://demo-api.ig.com/gateway/deal",
+    "prod": "https://api.ig.com/gateway/deal",
+}
+
+API_KEYS = {
+    "demo": IG_DEMO_API_KEY,
+    "prod": IG_PROD_API_KEY,
+}
+
+IDENTIFIERS = {
+    "demo": IG_DEMO_IDENTIFIER,
+    "prod": IG_PROD_IDENTIFIER,
+}
+
+PASSWORDS = {
+    "demo": IG_DEMO_PASSWORD,
+    "prod": IG_PROD_PASSWORD,
+}
